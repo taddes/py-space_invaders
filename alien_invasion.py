f86713ec88940fa86515c38eaaa92c6a3637a49b
Taddes Korris <taddes.korris@passportinc.com>
2019-08-02 16:11:06 -0400

    refactor and modularize

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,3 @@
-import sys
 
 import pygame
 from pygame.sprite import Group
@@ -8,29 +7,26 @@ from ship import Ship
 import game_functions as gf
 
 def run_game():
-    """Initialize game and create a screen object"""
+    # Initialize pygame, settings, and screen object.
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode(
-            (ai_settings.screen_width, ai_settings.screen_height))
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
-    # Create ship
+    
+    # Set the background color.
+    bg_color = (230, 230, 230)
+    
+    # Make a ship.
     ship = Ship(ai_settings, screen)
-    #  Make a group to store bullets
+    # Make a group to store bullets in.
     bullets = Group()
     
-    screen.fill(ai_settings.bg_color)
-
-    """Initialte game loop"""
+    # Start the main loop for the game.
     while True:
-        # Watch for keyboard or mouse events
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         bullets.update()
         gf.update_screen(ai_settings, screen, ship, bullets)
-
         
-        
-
 run_game()
